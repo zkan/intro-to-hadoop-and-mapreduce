62817ee10f17718559dcbcdd435cf33000883a4d
prontokan <kan@prontomarketing.com>
2014-03-08 20:43:18 +0700

    initialize the code for post and answer length exercise (just copy the code from previous exercise)

---
+++ code/average_length_reducer.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import csv
+import sys
+
+reader = csv.reader(sys.stdin, delimiter='\t')
+writer = csv.writer(
+    sys.stdout,
+    delimiter='\t',
+    quotechar='"',
+    quoting=csv.QUOTE_ALL
+)
+
+old_key = None
+hours = []
+hours_counter = {}
+
+for line in reader:
+    # Process only the valid lines.
+    if len(line) > 0:
+        author_id, hour = line
+
+        this_key = author_id
+        if old_key and old_key != this_key:
+            for hour in hours:
+                if hour in hours_counter:
+                    hours_counter[hour] += 1
+                else:
+                    hours_counter[hour] = 1
+
+            # Sort the popular hours in descending order.
+            popular_hours = sorted(
+                hours_counter,
+                key=hours_counter.get,
+                reverse=True
+            )
+
+            results = [author_id, popular_hours[0]]
+            writer.writerow(results)
+
+            hours = []
+            hours_counter = {}
+
+        old_key = this_key
+        hours.append(hour)
+
+if old_key != None:
+    for hour in hours:
+        if hour in hours_counter:
+            hours_counter[hour] += 1
+        else:
+            hours_counter[hour] = 1
+
+    # Sort the popular hours in descending order.
+    popular_hours = sorted(
+        hours_counter,
+        key=hours_counter.get,
+        reverse=True
+    )
+
+    results = [author_id, popular_hours[0]]
+    writer.writerow(results)
